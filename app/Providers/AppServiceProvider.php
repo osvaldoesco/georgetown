6518eb7dd9d3b495d38464c32248113447def3f0
Osvaldo Escobar <eoeg1410@gmail.com>
2019-05-15 07:20:21 -0600

    Solving issue with asset

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\Facades\URL;
+use Illuminate\Routing\UrlGenerator;
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\Schema;
 
@@ -23,7 +24,7 @@ class AppServiceProvider extends ServiceProvider
      *
      * @return void
      */
-    public function boot(){
+    public function boot(UrlGenerator $url){
       if(env('APP_ENV') === 'production')
       {
         $url->forceSchema('https');
