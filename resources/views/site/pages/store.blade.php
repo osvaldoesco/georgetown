e616a65ba44b5d61af84482c73f091aa768defc3
Osvaldo Escobar <eoeg1410@gmail.com>
2021-01-15 14:22:22 -0600

    Add style changes and fixed to store

--- resources/views/site/pages/store.blade.php
+++ resources/views/site/pages/store.blade.php
@@ -1,15 +1,15 @@
 @extends('layout.site', ['page' => 'store'])
 
 @section('content')
-  <section class="events__list-container">
+  <section class="events__list-container products-container">
     <div class="container padding-vertical">
       <div class="row">
         @foreach ($products as $product)
-          <div class="col-12 col-lg-3">
+          <div class="col-12 col-md-6 col-lg-4 col-xl-3">
             <div class="events-box mb-25">
               <a href="{{ route('pages.product', ['id' => $product->id]) }}">
-                <div class="product-image" style="background-image: url({{ asset($product->image) }});">
-                </div>
+                <div class="product-image product-image-lg d-none d-md-block" style="background-image: url({{ asset($product->image) }});"></div>
+                <img class="product-image-xs d-md-none" src="{{ asset($product->image) }}" alt="{{$product->title}}"/>
               </a>
               <div class="events__caption product__caption">
                 <a href="{{ route('pages.product', ['id' => $product->id]) }}">
